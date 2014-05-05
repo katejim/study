0e22812db6b6111fad099a84381d9efcb5351773
katty <katty@katty.(none)>
2014-05-05 15:09:59 +0400

    refactor

--- algo/timus/1439/harryTimus.cpp
+++ algo/timus/1439/harryTimus.cpp
@@ -6,153 +6,183 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-struct node
+struct AVLTreeNode
 {
     int key;
-    node* left;
-    node* right;
+    AVLTreeNode * left;
+    AVLTreeNode * right;
 
     int size;
     int height;
 
-    node(int m_key) : key(m_key), left(0), right(0), size(1), height(1) {}
-
-} *tree;
-
-unsigned char get_height(node* p)
-{
-    return p ? p -> height : 0;
-}
-int get_size(node *p)
-{
-    return p ? p -> size : 0;
-}
-
-int diff(node* p)
-{
-    return get_height(p -> right) - get_height(p -> left);
-}
-
-void fix_height(node* p)
-{
-    int height_left_subtree = get_height(p -> left);
-    int height_right_subtree = get_height(p -> right);
-    if(height_left_subtree > height_right_subtree)
-        p -> height = height_left_subtree;
-    else
-        p -> height = height_right_subtree;
-    ++p -> height;
-}
-
-node* rotate_right(node* p)
-{
-    node* q = p -> left;
-    p -> left = q -> right;
-    q -> right = p;
-    fix_height(p);
-    fix_height(q);
-
-    q -> size = p -> size;
-    p -> size = get_size(p -> left) + get_size(p -> right) + 1;
-
-    return q;
-}
-
-node* rotate_left(node* q)
-{
-    node* p = q -> right;
-
-    q -> right = p -> left;
-    p -> left = q;
-
-    fix_height(q);
-    fix_height(p);
-
-    p -> size = q -> size;
-    q -> size = get_size(q -> left) + get_size(q -> right) + 1;
-
-    return p;
-}
-
-node* balance(node* p)
-{
-    fix_height(p);
-    if( diff(p) == 2 )
+    explicit AVLTreeNode(int m_key) :
+        key(m_key), left(0), right(0), size(1), height(1)
     {
-        if( diff(p -> right) < 0 )
-            p -> right = rotate_right(p -> right);
-        return rotate_left(p);
     }
-    if( diff(p) == -2 )
-    {
-        if( diff( p -> left) > 0  )
-            p -> left = rotate_left(p -> left);
-        return rotate_right(p);
-    }
-    return p;
-}
+};
 
-
-node* insert(node* p, int key)
+class BattleWithYouKnowWho
 {
-    if( !p )
-        return new node(key);
-    if( key < p->key )
-        p->left = insert(p -> left, key);
-    else
-        p->right = insert(p->right, key);
-    ++p->size;
 
-    return balance(p);
-}
+  public:
+    BattleWithYouKnowWho():
+        tree(0)
+    {
+    }
 
-int get_real_position(int inQuery) {
-    inQuery;
-    int leftCount = 0;
-    int rightCount;
-    node *p = tree;
-    int size_left_subtree;
-    while (p != NULL) {
-        size_left_subtree = p->key - leftCount - get_size(p->left);
-        if( inQuery < size_left_subtree)
-            p  = p->left;
-        else {
-            inQuery -= size_left_subtree;
-            leftCount = p->key + 1;
-            p = p->right;
+    int get_real_room(int in_query)
+    {
+        int left_count = 0;
+        AVLTreeNode * p = tree;
+
+        while (p != NULL)
+        {
+            int free_rooms = p->key - left_count - get_size(p->left);
+
+            if( in_query < free_rooms)
+                p = p->left;
+            else
+            {
+                in_query -= free_rooms;
+                left_count = p->key + 1;
+                p = p->right;
+            }
+        }
+
+        return in_query + left_count;
+    }
+
+    void delete_room(size_t room_number)
+    {
+        tree = insert(tree, get_real_room(room_number));
+    }
+
+    ~BattleWithYouKnowWho()
+    {
+        delete_node(tree);
+    }
+
+
+  private:
+    AVLTreeNode * tree;
+
+    int get_height(AVLTreeNode * const p)
+    {
+        return p ? p->height : 0;
+    }
+    int get_size(AVLTreeNode * const p)
+    {
+        return p ? p->size : 0;
+    }
+
+    int diff(AVLTreeNode * const p)
+    {
+        return get_height(p->right) - get_height(p->left);
+    }
+
+    void fix_height(AVLTreeNode * p)
+    {
+        int height_left_subtree = get_height(p->left);
+        int height_right_subtree = get_height(p->right);
+
+        if(height_left_subtree > height_right_subtree)
+            p->height = height_left_subtree;
+        else
+            p->height = height_right_subtree;
+
+        ++p->height;
+    }
+
+    AVLTreeNode * rotate_right(AVLTreeNode * p)
+    {
+        AVLTreeNode * q = p->left;
+        p->left = q->right;
+        q->right = p;
+        fix_height(p);
+        fix_height(q);
+        q->size = p->size;
+        p->size = get_size(p->left) + get_size(p->right) + 1;
+        return q;
+    }
+
+    AVLTreeNode * rotate_left(AVLTreeNode * q)
+    {
+        AVLTreeNode * p = q->right;
+        q->right = p->left;
+        p->left = q;
+        fix_height(q);
+        fix_height(p);
+        p->size = q->size;
+        q->size = get_size(q->left) + get_size(q->right) + 1;
+        return p;
+    }
+
+    AVLTreeNode * balance(AVLTreeNode * p)
+    {
+        fix_height(p);
+
+        if( diff(p) == 2 )
+        {
+            if( diff(p->right) < 0 )
+                p->right = rotate_right(p->right);
+
+            return rotate_left(p);
+        }
+
+        if( diff(p) == -2 )
+        {
+            if( diff( p->left) > 0 )
+                p->left = rotate_left(p->left);
+
+            return rotate_right(p);
+        }
+
+        return p;
+    }
+
+
+    AVLTreeNode * insert(AVLTreeNode * p, int key)
+    {
+        if( !p )
+            return new AVLTreeNode(key);
+
+        if( key < p->key )
+            p->left = insert(p->left, key);
+        else
+            p->right = insert(p->right, key);
+
+        ++p->size;
+        return balance(p);
+    }
+
+    void delete_node(AVLTreeNode * p)
+    {
+        if (p != NULL)
+        {
+            delete_node(p->left);
+            delete_node(p->right);
+            delete p;
         }
     }
-    return inQuery + leftCount;
-}
-
-int rezult(char letter, int current_room_number) {
-    int rez = -1;
-    if (letter == 'D')
-        tree = insert(tree, get_real_position(current_room_number));
-    else if (letter == 'L')
-        rez = get_real_position(current_room_number);
-    return rez;
-}
+};
 
 int main()
 {
-    int allNumbers, allLines;
-    cin >> allNumbers >>allLines;
+    int all_numbers, all_lines;
+    cin >> all_numbers >> all_lines;
+    BattleWithYouKnowWho battle;
 
-    tree = NULL;
+    for(int i = 0; i != all_lines; ++i)
+    {
+        char action_letter;
+        int room_number;
+        cin >> action_letter >> room_number;
 
-    vector <int> rez;
-    for(int i = 0; i != allLines; ++i) {
-        char actionLetter;
-        int roomNumber;
-        cin >> actionLetter >> roomNumber;
-        int temp = rezult(actionLetter, roomNumber);
-        if (temp != -1)
-            rez.push_back(temp);
+        if (action_letter == 'D')
+            battle.delete_room(room_number);
+        else if (action_letter == 'L')
+            cout << battle.get_real_room(room_number) << endl;
     }
 
-    int size = rez.size();
-    for(int i = 0; i < size ; ++i)
-        cout << rez[i] << endl;
     return 0;
 }
